2366872e71364457fb60a093b4820c9d77ca511b
Admin <Admin@DESKTOP-EKMKES5>
2022-12-27 15:57:00 +0530

    build 109

---
+++ rest_api/src/r_api/post1.java
@@ -0,0 +1,29 @@
+package r_api;
+
+import org.json.simple.JSONObject;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class post1 {
+
+	public static void main(String[] args) {
+	RestAssured.baseURI="https://reqres.in";
+	RequestSpecification r = RestAssured.given();
+	JSONObject obj = new JSONObject();
+obj.put("name", "morpheus");
+obj.put("job", "leader");
+r.contentType(ContentType.JSON);
+r.body(obj.toJSONString());
+
+Response res = r.request(Method.POST,"api/users");
+System.out.println(res.asString());
+int code = res.statusCode();
+System.out.println(code);
+
+	}
+
+}
